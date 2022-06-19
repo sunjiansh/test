03120d341bef81849291580a95889dd947662abf
sunjiansh <sunjian0928@126.com>
2022-06-19 15:11:58 +0800

    init commit

---
+++ src/TT.java
@@ -0,0 +1,11 @@
+/**
+ * @author sunjian
+ * @Date 2022/6/19
+ * @company
+ * @license 版权所有，非经许可请勿使用本代码
+ */
+public class TT {
+    public static void main(String[] args) {
+        System.out.println("xxxxxxxxxxx");
+    }
+}
